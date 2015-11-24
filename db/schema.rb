9026411656f04a0eb7616a94a0db05ba6b1026c4
krling13 <kirsten.ling@yale.edu>
2015-11-23 23:57:26 -0600

    hw8

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20151119055824) do
+ActiveRecord::Schema.define(version: 20151124014236) do
 
   create_table "comments", force: :cascade do |t|
     t.integer  "photo_id"
